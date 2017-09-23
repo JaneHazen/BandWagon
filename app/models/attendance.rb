ebbc079bba8d3c70fd0e323830f77fd757419585
Jane <iamjane.gmail.com>
2017-09-23 13:20:16 -0700

    fixed models/migrations somewhat

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,4 @@
 class Attendance < ApplicationRecord
-  belongs_to :attendee, class_name: "User", foreign_key: "attendee"
-  belongs_to :concert, foreign_key: "concert"
+  belongs_to :attendee, class_name: "User", foreign_key: "attendee_id"
+  belongs_to :concert, foreign_key: "concert_id"
 end
