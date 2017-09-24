09002653f222e3e1e76038bda7937d58b8120092
Jane <iamjane.gmail.com>
2017-09-24 10:46:55 -0700

    add validations, more css

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_many :attendances, foreign_key: "attendee"
   has_many :concerts, through: :attendances
 
+  validates :first_name, :last_name, :email, {presence: true}
+  validates :email, {uniqueness: true}
+  validate :has_password
+
   has_secure_password
 end
--- db/migrate/20170923190432_create_concerts.rb
+++ db/migrate/20170923190432_create_concerts.rb
@@ -1,10 +1,10 @@
 class CreateConcerts < ActiveRecord::Migration[5.0]
   def change
     create_table :concerts do |t|
-      t.string :band
-      t.string :venue
-      t.date :date
-      t.string :start_time
+      t.string :band, {null: false}
+      t.string :venue, {null: false}
+      t.date :date, {null: false}
+      t.string :start_time, {null: false}
 
       t.timestamps
     end
