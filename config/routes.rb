b0236488c16fd7aa5a6ef4c5c6b2975bf6a1674b
Jane <iamjane.gmail.com>
2017-09-23 17:20:08 -0700

    login/logout all working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'concerts#index'
   resources :concerts, only: [:index, :show]
   resources :users, only:[:new, :create, :show]
+  resources :sessions, only:[:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
