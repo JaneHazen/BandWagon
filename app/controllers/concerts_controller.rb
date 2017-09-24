bf4f7f6444dfd79c76fdcb2e030c0a02c0d2826f
Jane <iamjane.gmail.com>
2017-09-23 20:53:11 -0700

    concert show page has attendees now

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -6,5 +6,6 @@ class ConcertsController < ApplicationController
 
   def show
     @concert = Concert.find(params[:id])
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
